a36a97699ecb71ea7075ec80f03c8540f100dcff
Adam Bray <adam.bray@yale.edu>
2015-11-03 12:47:32 -0500

    Added feedback

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// I think this is a great way to structure your data!
 cardData = [{
   front: "שלום",
   back: "Hello (Peace)"
@@ -28,6 +29,7 @@ function drawCard() {
   $("#backOfCard").hide();
 }
 
+// I like that you renamed this function to be more active and readable!
 function advanceCard() {
   if (currentNumber < (cardData.length - 1)) {
     currentNumber++;
@@ -46,6 +48,7 @@ function reverseCard() {
 }
 
 function initializeGame() {
+  // is there a reason for this if statement?
   if (($("#frontOfCard").text() === "") && ($("#backOfCard").text() === "")) {
     drawCard();
     toggleButtons();
@@ -75,6 +78,9 @@ function markCorrect() {
   if (currentNumber < (cardData.length - 1)) {
     removeCard();
   } else if ((currentNumber <= (cardData.length - 1)) && (cardData.length > 1)) {
+    // the conditional above definitely needs a comment. I can't quite figure
+    // out why it's there, and why you have to reset the currentNumber to zero
+    // in this case...
     cardData.splice(currentNumber, 1);
     currentNumber = 0;
     drawCard();
@@ -89,6 +95,8 @@ function markCorrect() {
 function markWrong() {
   $("#backOfCard").css("background-color", "red");
   cardData[currentNumber].correct = false
+  // do you use this property anywhere? I thought the idea was to use this
+  // property in `drawCard()` to decide what color to draw the card as.
 }
 
 $("#start").on("click", initializeGame);
